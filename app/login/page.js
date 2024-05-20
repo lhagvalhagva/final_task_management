6b3ab057e846c09828523b97b75e5ae14a6f5c7a
ganchimeg <ganchmq@gmail.com>
2024-05-20 23:12:32 +0800

    gana

--- app/login/page.js
+++ app/login/page.js
@@ -3,6 +3,7 @@
 import { useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import './module.css';
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
