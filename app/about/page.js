0ea11ae0a62fc15a5ed261c5d9220a039758b6fe
Dorjnyam Erdenetsogt <mjldoko11@gmail.com>
2024-05-26 16:43:01 +0800

    footer and about2

---
+++ app/about/page.js
@@ -0,0 +1,17 @@
+'use client';
+
+import Navbar from "../../components/NavBar";
+import Footer from "../../components/Footer";
+import styles from "./about.css";
+
+export default function AboutPage() {
+    return (
+        <div>
+            <Navbar />
+            <div className="main">
+                өө манай багийн талаар юм орноо. хха
+            </div>
+            <Footer />
+        </div>
+    );
+}
